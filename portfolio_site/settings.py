334e2f029d3129d429fa39b3baa3162dc41f6de2
Tahir Ismailwala <tahir.va3@gmail.com>
2025-08-01 16:43:59 -0400

    Update settings.py for local run or deployment config

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
